b987ad5901a2104aaf0692d08acc85129cdca149
Ben Salinaro <MY_NAME@example.com>
2021-07-12 16:30:33 -0500

    adf

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
     <Router>
       <NavigationWithRouter />
       <Switch>
-        <Route path="/" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/React-Portfolio/" component={About} />
+        <Route exact path="/React-Portfolio/portfolio" component={Portfolio} />
+        <Route exact path="/React-Portfolio/contact" component={Contact} />
       </Switch>
     </Router>
     <Footer />
