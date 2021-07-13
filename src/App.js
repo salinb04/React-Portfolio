4401ad7e2d9d513d4ee667c09de2ce83ae2ac22e
Ben Salinaro <MY_NAME@example.com>
2021-07-13 14:00:13 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       <NavigationWithRouter />
       <Switch>
         <Route exact path="/React-Portfolio/" component={About} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </Router>
     <Footer />
