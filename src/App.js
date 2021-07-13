6fc9140b3f844e9fe4ccfe8c883f1351b597d47e
Ben Salinaro <MY_NAME@example.com>
2021-07-13 17:50:53 -0500

    added script to index.html and made a 404.html page in public folder

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       <NavigationWithRouter />
       <Switch>
         <Route exact path="/React-Portfolio/" component={About} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/React-Portfolio/portfolio" component={Portfolio} />
+        <Route exact path="/React-Portfolio/contact" component={Contact} />
       </Switch>
     </Router>
     <Footer />
