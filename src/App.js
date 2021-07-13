9c59f6354ddda59385fabe90ef6f4bc52c803126
Ben Salinaro <MY_NAME@example.com>
2021-07-13 18:33:37 -0500

    added basename property to router on app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import NavigationWithRouter from "./components/navigation/Navigation";
 import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio";
@@ -9,14 +9,14 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div>
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <NavigationWithRouter />
       <Switch>
         <Route exact path="/" component={About} />
         <Route exact path="/Portfolio" component={Portfolio} />
         <Route exact path="/Contact" component={Contact} />
       </Switch>
-    </Router>
+    </HashRouter>
     <Footer />
     </div>
   );
